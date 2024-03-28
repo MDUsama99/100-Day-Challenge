f729f68d7f60d3e300fb3274c4d1c66beb3de29e
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 17:09:41 +0500

    Add files via upload

---
+++ Day 16/Q46/index.ts
@@ -0,0 +1,9 @@
+let laptop = {
+    make: "Dell",
+    model: "XPS 15",
+    year: "2022",
+    describe: function() {
+        console.log(`This laptop is a ${this.year} ${this.make} ${this.model}.`);
+    }
+};
+laptop.describe();
