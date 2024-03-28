891d2e32b8d179c46d86e1ea99b605a4868067bb
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 17:12:15 +0500

    Add files via upload

---
+++ Day 16/Advance array/AdvanceArray.js
@@ -0,0 +1,8 @@
+var laptops = [
+    { make: "Dell", model: "XPS 15", year: "2021" },
+    { make: "Apple", model: "Macbook Pro", year: "2022" },
+    { make: "HP", model: "Spectre x360", year: "2023" }
+];
+var laptop1 = laptops[0], laptop2 = laptops[1];
+console.log(laptop1);
+console.log(laptop2);
